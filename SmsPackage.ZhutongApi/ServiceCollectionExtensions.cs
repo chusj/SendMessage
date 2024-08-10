4f7647a1294eb11cbabf64108045d4984c4f97aa
chusj <chusj.mail@qq.com>
2024-08-10 15:08:46 +0800

    添加助通Api类库

---
+++ SmsPackage.ZhutongApi/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using SmsPackage.Model;
+using SmsPackage.Service;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddZhutongSendMessageApi(this IServiceCollection services, Action<ZhutongOptions> configure)
+        {
+            services.AddOptions<ZhutongOptions>().Configure(configure);
+            services.AddTransient<IZhutongService, ZhutongSendService>();
+            return services;
+        }
+    }
+}
