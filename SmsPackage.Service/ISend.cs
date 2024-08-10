72d1393604ed4dbffa81ae9f7c53650ee0db8717
chusj <chusj.mail@qq.com>
2024-08-10 12:42:28 +0800

    新增服务层、服务接口

---
+++ SmsPackage.Service/ISend.cs
@@ -0,0 +1,14 @@
+﻿namespace SmsPackage.IService
+{
+    public interface ISend
+    {
+        /// <summary>
+        /// 发送
+        /// </summary>
+        /// <param name="mobileList">手机号码</param>
+        /// <param name="content">内容</param>
+        /// <param name="suffix">签名</param>
+        /// <returns></returns>
+        Task<SmsResponse> Send(List<string> mobileList, string content, string suffix);
+    }
+}
