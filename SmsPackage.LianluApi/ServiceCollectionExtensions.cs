f991ad39680b7c748bab8841d5efdbdd3d2edf4e
chusj <chusj.mail@qq.com>
2024-08-10 15:25:49 +0800

    联麓短信，添加服务拓展方法

---
+++ SmsPackage.LianluApi/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using SmsPackage.Model;
+using SmsPackage.Service;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 添加联麓发送短信api服务
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configure"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddLianluSendMessageApi(this IServiceCollection services, Action<LianluOptions> configure)
+        {
+            services.AddOptions<LianluOptions>().Configure(configure);
+            services.AddTransient<ILianluService, LianluSendService>();
+            return services;
+        }
+    }
+}
