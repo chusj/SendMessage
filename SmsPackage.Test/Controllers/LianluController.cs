8469f811a148e806d2728bf4defcca04e4825199
chusj <chusj.mail@qq.com>
2024-08-16 15:18:06 +0800

    类库dll已经上传到nuget，将项目引用更换为nuget包引用

--- SmsPackage.Test/Controllers/LianluController.cs
+++ SmsPackage.Test/Controllers/LianluController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SmsPackage.Model;
 using SmsPackage.Service;
 
@@ -30,7 +29,7 @@ namespace SmsPackage.Test.Controllers
         public async Task<LianLuApiResponse> Test(string mobile)
         {
             var mobiles = new List<string>() { mobile };
-            return await _lianluService.Send(mobiles, "L您好，今天您的生日，祝您生日快乐。 温馨提醒：为了您的健康请定期进行健康体检。", "【杭州希和】");
+            return await _lianluService.Send(mobiles, "您好，今天您的生日，祝您生日快乐。 温馨提醒：为了您的健康请定期进行健康体检。", "【杭州希和】");
         }
     }
 }
