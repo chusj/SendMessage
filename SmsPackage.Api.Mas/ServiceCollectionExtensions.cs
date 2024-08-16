673b2865210853337d929a363cb39dec083e080c
chusj <chusj.mail@qq.com>
2024-08-16 16:16:20 +0800

    添加MAS服务拓展类

---
+++ SmsPackage.Api.Mas/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using SmsPackage.Model;
+using SmsPackage.Service;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 添加中国移动云MAS短信服务
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configure"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMasSendMessageApi(this IServiceCollection services, Action<MasOptions> configure)
+        {
+            services.AddOptions<MasOptions>().Configure(configure);
+            services.AddTransient<IMasService, MasSendService>();
+            return services;
+        }
+    }
+}
