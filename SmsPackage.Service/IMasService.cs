72e2c263cfd020cc98ff9a2567356a67d4ac4c13
chusj <chusj.mail@qq.com>
2024-08-16 16:15:42 +0800

    添加MAS平台服务接口和服务实现类

---
+++ SmsPackage.Service/IMasService.cs
@@ -0,0 +1,17 @@
+﻿using SmsPackage.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmsPackage.Service
+{
+    public interface IMasService
+    {
+        /// <summary>
+        /// 发送
+        /// </summary>
+        /// <param name="mobileList">手机号码</param>
+        /// <param name="content">内容</param>
+        /// <returns></returns>
+        Task<MasApiResponse> Send(List<string> mobileList, string content);
+    }
+}
---
+++ SmsPackage.Service/MasSendService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmsPackage.Service
+{
+    public class MasService : IMasService
+    {
+    }
+}
